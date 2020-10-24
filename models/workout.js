31bc1f1e157ab13d062e9964bedad85f87e3592f
Adam Jones <adam.w.jones2@gmail.com>
2020-10-24 14:03:38 -0400

    updated created models and server.js

---
+++ models/workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
+    // totalDuration: Number,
+    exercises: [
+      {
+        type: {
+          type: String,
+          enum: ['cardio', 'resistance'],
+          required: "Enter an exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name",
+        },
+        duration: {
+          type: Number,
+          trim: true,
+          required: "Enter the duration in minutes",
+        },
+        weight: {
+          type: Number,
+          required: function(){
+              return this.type === "resistance"
+          }
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((a, x) => (a += x.duration), 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+module.exports = router;
+
+
+// In 17-7/Solved/server.js has an outline of HTML...or they are Mongo routes...IDK 
